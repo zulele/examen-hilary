77d0b844c02ab50f4d34eb6b6c16e45a820bf6e6
zulele <hilary78munguia@gmail.com>
2017-03-10 12:15:45 -0600

    examen progra

---
+++ funciones.js
@@ -0,0 +1,7 @@
+var baco.b = angular.module
+
+
+var=cantidad1
+var=cantidad2
+var=cantidad1+cantidad2
+
---
+++ script.js
@@ -0,0 +1,5 @@
+angular.module ("baco.b"[); controller ("mensaje,function($scope[]);
+angular module ("baco.b"[); controller ("operaciones,function($scope[]);
+
+<script sr= funciones.js 
+
